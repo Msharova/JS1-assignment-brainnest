33675ce8ad26c5b563bef65e0c2ec7ff159510d7
Maria Sharova <sharova.maria.3d@gmail.com>
2023-02-11 16:01:32 +0100

    the js logic correction

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function computerPlay(){
+function computerPlay() {
     const gameWords =[`Rock`, `Paper`, `Scissors`]; 
     return gameWords[Math.floor(Math.random() * gameWords.length)];
 }
 
-function playRound(playerSelection, computerSelection){    
+function determineWinner(playerSelection, computerSelection) {    
     switch (playerSelection) {
         case `rock`:
             if (computerSelection === `paper`) return loseGame;
@@ -25,23 +25,41 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
-function game(roundCount){
-    let playerWin = 0;
-    let compWin = 0;
+function playerInput() {
+    let questionForPlayer = prompt(`Let's play! Type down "Rock", "Paper" or "Scissors"`);
 
-    for (let i = 0; i<roundCount; i++) {
-        const questionForPlayer = prompt(`Let's play! Write down "Rock", "Paper" or "Scissors"`);
-        const playerSelection = questionForPlayer.toLowerCase();
-        const computerSelection = computerPlay().toLowerCase();
-
-
-        console.log(`Round ${i+1}: Computer selected: ${capitalizeFirstLetter(computerSelection)}, you selected ${capitalizeFirstLetter(playerSelection)}`);
-
-        let result = playRound(playerSelection,computerSelection);
-
-        if (result === winGame) playerWin++;
-        if (result === loseGame) compWin++;
+    while (questionForPlayer === null) {
+        let endGame = confirm("Do you whish to end the game?");
+        if (endGame)
+            return noGame;
+        else 
+            questionForPlayer = prompt(`Let's play! Type down "Rock", "Paper" or "Scissors"`);
     }
+
+    let playerSelection = questionForPlayer.toLowerCase().trim();
+
+    if (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
+       alert(`Invalid input. Please select "Rock", "Paper" or "Scissors"`);
+       return playerInput();
+    }
+
+    return playerSelection;
+}
+
+function gameRound(index) {
+    const playerSelection = playerInput();
+    
+    if (playerSelection === noGame) return noGame;
+    
+    const computerSelection = computerPlay().toLowerCase();
+    
+    let result = determineWinner(playerSelection,computerSelection);
+    console.log(`Round ${index+1}: Computer selected: ${capitalizeFirstLetter(computerSelection)}, you selected ${capitalizeFirstLetter(playerSelection)}. ${result}`);
+   
+    return result;
+}
+
+function printResults(playerWin, compWin, roundCount) {
     if (playerWin>compWin){
         console.log(`You played ${roundCount} games, won ${playerWin}, lost ${compWin}. You are the winner!`);
     }
@@ -53,15 +71,28 @@ function game(roundCount){
     }
 }
 
-function capitalizeFirstLetter(wordSelected){
+function game(roundCount) {
+    let playerWin = 0;
+    let compWin = 0;
+    let i =0;
+
+    for (; i<roundCount; i++) {
+        
+        let result = gameRound(i);
+        if (result === winGame) playerWin++;
+        if (result === loseGame) compWin++;
+        if (result === noGame) break;
+    }
+    printResults(playerWin, compWin, i);
+}
+
+function capitalizeFirstLetter(wordSelected) {
     return wordSelected.charAt(0).toUpperCase() + wordSelected.slice(1);
 }
 
 let winGame = `You win!`;
 let loseGame = `You lose!`;
 let tieGame = `It's a tie!`;
-let noGame = `You don't want to play?`
+let noGame = `See you next time!`;
 
 game(5);
-
-
